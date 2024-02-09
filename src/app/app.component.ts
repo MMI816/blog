812370b22ef859631d4ed649b801f8c61e4ce9df
Maria Moreno Ibáñez <mariamorenoibanez@gmail.com>
2024-02-09 13:50:41 +0100

    dando estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,16 +14,16 @@ import { NoticesComponent } from './components/notices/notices.component';
 export class AppComponent {
   arrNoticias: INoticia[] = [
     {
-      title: "",
-      image: "",
-      text: "",
-      date: 0,
+      title: "La lucha CONTRA EL DESPERDICIO ALIMENTARIO ganó la partida en Madrid Fusión 2024",
+      image: "https://www.cuentamealgobueno.com/wp-content/uploads/2024/02/coffeetogo-3926395_640.jpg",
+      text: "El cocinero valenciano Ricard Camarena recibió el premio “Defensa del Producto Alimentos de España” durante la celebración de la vigésima edición Madrid Fusion. Este reconoce su lucha contra el desperdicio alimentario. Esto ocurrió en el Congreso Internacional de Alta Cocina Madrid Fusión Alimentos de España y el Ministro de Agricultura, Pesca y Alimentación, Luis Planas, acudió al evento para hacer entrega de este galardón.",
+      date: "8 febrero 2024",
     },
     {
-      title: "",
-      image: "",
-      text: "",
-      date: 0,
+      title: "La APP de voluntariado VOLUNCLOUD bate el récord de usuarios",
+      image: "https://www.cuentamealgobueno.com/wp-content/uploads/2024/02/20240206-voluncloud-1024x640.jpg",
+      text: "En enero, Voluncloud, la app que une a personas y ONG que necesitan voluntariado, ha rozado las 6.000 inscripciones ciudadanas y cuenta ya con 315 entidades sociales registradas en todo el Estado, lo que supone la cifra más alta de su historia.Según el último informe de la Plataforma del Voluntariado (PVE), editado el pasado mes de diciembre, en España hay más 4,5 millones de personas que hacen voluntariado.",
+      date: "6 febrero 2024",
     }
   ]
 
